8f83742fa32a384b007abbc8375e36d8ba84700e
rezaanandap <89363412+rezaanandap@users.noreply.github.com>
2026-02-13 11:41:01 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "absensi-pwa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-gold.png",
+  
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((c) => c || fetch(event.request)));
+});
